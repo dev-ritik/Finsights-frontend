689a41075bfefef61fcc10fb8387bfc34087e5f1
dev-ritik <ritikkne@gmail.com>
2022-01-19 00:49:14 +0530

    :sparkles: Add chart in public wishlist

--- app/routes/Dashboards/PublicWishlist/PublicWishlistItem.js
+++ app/routes/Dashboards/PublicWishlist/PublicWishlistItem.js
@@ -1,20 +1,49 @@
 import React from 'react';
 
-import {CardBody, CardFooter, Progress, Table} from './../../../components';
+import {
+    CardBody,
+    CardFooter,
+    CardTitle,
+    Nav,
+    NavItem,
+    NavLink,
+    Progress,
+    Table,
+    TabPane,
+    UncontrolledTabs,
+    UncontrolledTooltip
+} from './../../../components';
 import PropTypes from "prop-types";
-import {CardTitle, UncontrolledTooltip} from "reactstrap";
-
+import {
+    CartesianGrid,
+    Legend,
+    Line,
+    LineChart,
+    ReferenceLine,
+    ResponsiveContainer,
+    Tooltip,
+    XAxis,
+    YAxis,
+} from 'recharts';
+import moment from "moment";
 
 const PublicWishlistItem = (props) => {
     const stocks = props.stocks.filter(item => item.stock_id === props.data.stock_id).map(({
                                                                                                name,
                                                                                                symbol,
-                                                                                               price
+                                                                                               price,
+                                                                                               ticker_last_checked,
                                                                                            }) => ({
         name,
         symbol,
         price,
+        ticker_last_checked,
     }));
+
+    let stock = null;
+    if (stocks && stocks.length === 1) {
+        stock = stocks[0]
+    }
 
     const buyQuantityDisplay = (buy_price, buy_piece) => {
         return `${buy_price && buy_piece && Number(buy_piece) > 0 && Number(buy_price) > 0 ?
@@ -24,13 +53,101 @@ const PublicWishlistItem = (props) => {
             )}`
     }
 
+    const WishlistTable = () => {
+        return (
+            <Table className="mb-0" hover responsive>
+                <thead>
+                <tr>
+                    <th className="bt-0">Action</th>
+                    <th className="bt-0">Price(₹)</th>
+                    <th className="bt-0">Quantity %</th>
+                    <th className="bt-0">Confidence</th>
+                    <th className="bt-0">Execute by</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Buy</td>
+                    <td>{props.data.buy_price || "-"}</td>
+                    <td>{buyQuantityDisplay(props.data.buy_price, props.data.buy_piece)}</td>
+                    <td style={{width: '20%'}}>
+                        <Progress value={props.data.buy_feeling || 0} style={{height: "10px"}} className="mb-2"
+                                  striped max={5}/>
+                    </td>
+                    <td>
+                        {props.data.buy_valid_till || "-"}
+                    </td>
+                </tr>
+                <tr>
+                    <td>Sell</td>
+                    <td>{props.data.sell_price || "-"}</td>
+                    <td>{props.data.sell_quantity ? `${props.data.sell_quantity} %` : "-"}</td>
+                    <td style={{width: '20%'}}>
+                        <Progress value={props.data.sell_feeling || 0} style={{height: "10px"}} className="mb-2"
+                                  striped max={5}/>
+                    </td>
+                    <td>
+                        {props.data.sell_valid_till || "-"}
+                    </td>
+                </tr>
+                <tr>
+                    <td>Hold</td>
+                    <td/>
+                    <td/>
+                    <td style={{width: '20%'}}>
+                        <Progress value={props.data.hold_feeling || 0} style={{height: "10px"}} className="mb-2"
+                                  striped max={5}/>
+                    </td>
+                    <td/>
+                </tr>
+                </tbody>
+            </Table>
+        )
+    }
+
+    const WishlistChart = () => {
+        const data = [
+            {
+                dt: moment(stock.ticker_last_checked).format("MMM Do YYYY, h:mm:ss a"),
+                price: stock.price,
+            },
+        ];
+
+        return (
+            <ResponsiveContainer width='100%'
+                                 minHeight='180px'
+            >
+                <LineChart
+                    width={500}
+                    height={300}
+                    data={data}
+                    margin={{top: 5, right: 20, left: 20, bottom: 5}}
+                >
+                    <CartesianGrid strokeDasharray="3 3"/>
+                    <XAxis dataKey="dt" padding={{left: 30, right: 30}}/>
+                    <YAxis type="number"/>
+                    <Tooltip/>
+                    <Legend/>
+                    <ReferenceLine strokeDasharray="3 3"
+                                   y={Number(props.data.sell_price)}
+                                   label="Sell"
+                                   stroke="green"
+                                   ifOverflow="extendDomain"/>
+                    <ReferenceLine strokeDasharray="3 3" y={Number(props.data.buy_price)} label="Buy" stroke="red"
+                                   ifOverflow="extendDomain"/>
+                    <Line type="monotone" dataKey="price" stroke="#8884d8"/>
+                </LineChart>
+            </ResponsiveContainer>
+        )
+    }
+
     return (
         <>
             <CardBody>
                 <div className="d-flex mb-2">
-                    {stocks && stocks.length === 1 && (<CardTitle tag="h4">
+                    {stock && (<CardTitle tag="h4">
                         <span
-                            className={`${props.data.executed ? "text-success" : ""}`}>{`${stocks[0].name}`}{stocks[0].price && (` . ₹${stocks[0].price}`)}</span>
+                            className={`${props.data.executed ? "text-success" : ""}`}>{`${stock.name}`}{stock.price && (` . ₹${stock.price}`)}</span>
                         {props.data.executed && (<>
                             <span> <i id="executed" className="fa fa-check-circle"/></span>
                             <UncontrolledTooltip placement="bottom" target="executed">
@@ -41,56 +158,45 @@ const PublicWishlistItem = (props) => {
                     </CardTitle>)}
 
                     <span className="ml-auto text-right">
-                    {props.data.exchange_id}{stocks && stocks.length === 1 && props.data.exchange_id && (`/`)}{stocks && stocks.length === 1 && (`${stocks[0].symbol}`)}
+                    {props.data.exchange_id}{stock && props.data.exchange_id && (`/`)}{stock && (`${stock.symbol}`)}
                 </span>
                 </div>
-                <Table responsive>
-                    <thead>
-                    <tr>
-                        <th>Action</th>
-                        <th>Price(₹)</th>
-                        <th>Quantity %</th>
-                        <th>Confidence</th>
-                        <th>Execute by</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>Buy</td>
-                        <td>{props.data.buy_price || "-"}</td>
-                        <td>{buyQuantityDisplay(props.data.buy_price, props.data.buy_piece)}</td>
-                        <td style={{width: '20%'}}>
-                            <Progress value={props.data.buy_feeling || 0} style={{height: "10px"}} className="mb-2"
-                                      striped max={5}/>
-                        </td>
-                        <td>
-                            {props.data.buy_valid_till || "-"}
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Sell</td>
-                        <td>{props.data.sell_price || "-"}</td>
-                        <td>{props.data.sell_quantity ? `${props.data.sell_quantity} %` : "-"}</td>
-                        <td style={{width: '20%'}}>
-                            <Progress value={props.data.sell_feeling || 0} style={{height: "10px"}} className="mb-2"
-                                      striped max={5}/>
-                        </td>
-                        <td>
-                            {props.data.sell_valid_till || "-"}
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Hold</td>
-                        <td/>
-                        <td/>
-                        <td style={{width: '20%'}}>
-                            <Progress value={props.data.hold_feeling || 0} style={{height: "10px"}} className="mb-2"
-                                      striped max={5}/>
-                        </td>
-                        <td/>
-                    </tr>
-                    </tbody>
-                </Table>
+                <UncontrolledTabs initialActiveTabId="data">
+                    <div className="d-flex">
+                        <Nav tabs>
+                            <NavItem>
+                                <UncontrolledTabs.NavLink tabId="data">
+                                    <i className="fa fa-fw fa-table mr-2"/>
+                                    Data
+                                </UncontrolledTabs.NavLink>
+                            </NavItem>
+                            <NavItem>
+                                {stock && stock.price &&
+                                    <UncontrolledTabs.NavLink tabId="chart">
+                                        <i className="fa fa-fw fa-bar-chart mr-2"/>
+                                        Chart
+                                    </UncontrolledTabs.NavLink>
+                                }
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href={`${stock ? `#/analysis/stock/NSE/${stock.symbol}/seasonal` : '#'}`}
+                                         target="_blank">
+                                    Analysis
+                                    <i className="fa fa-fw fa-external-link ml-2"/>
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                    </div>
+
+                    <UncontrolledTabs.TabContent>
+                        <TabPane tabId="data">
+                            <WishlistTable/>
+                        </TabPane>
+                        <TabPane tabId="chart">
+                            <WishlistChart/>
+                        </TabPane>
+                    </UncontrolledTabs.TabContent>
+                </UncontrolledTabs>
             </CardBody>
             {props.data.comment && props.data.comment !== "" && (
                 <CardFooter className="small">
