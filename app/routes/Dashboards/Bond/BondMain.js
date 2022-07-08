0cd383d9c37776f5c854e362d1c45b89b19e64c5
dev-ritik <ritikkne@gmail.com>
2022-07-09 02:34:36 +0530

    :sparkles: Add bond disclaimer

--- app/routes/Dashboards/Bond/BondMain.js
+++ app/routes/Dashboards/Bond/BondMain.js
@@ -15,6 +15,12 @@ class BondMain extends React.Component {
                     title={this.title}
                     className="mb-5 mt-4"
                 />
+                <ul className="mb-5">
+                    <li>Returns (and sort by returns) can be incorrect. <i>The numbers calculated here are for a natural
+                        cash-flow distribution. However, this may not be the case for all the bonds.</i>
+                    </li>
+                    <li>LTP and Volume information can be old</li>
+                </ul>
                 <Row>
                     <Col lg={12}>
                         <BondFilter {...this.props}/>
