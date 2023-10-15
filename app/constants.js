6e8b0588d7be50094755bcded24754aaa46054c5
dev-ritik <ritikkne@gmail.com>
2023-10-15 13:45:42 +0530

    :wrench: Update API_URL

--- app/constants.js
+++ app/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = `https://api.ritik.ga`
+export const API_URL = `https://fin.uaenorth.cloudapp.azure.com`
 // export const API_URL = `http://localhost:8000`
 export const POSTS_PER_PAGE = 10;
 export const ADMIN_EMAIL = "ritikkne@gmail.com";
